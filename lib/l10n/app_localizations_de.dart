6a86fe72246a66ce3579b3b4097343728b30f2c4
Woltan Schume <w.schume@gmail.com>
2026-02-26 21:40:15 +0100

    final fix

---
+++ lib/l10n/app_localizations_de.dart
@@ -0,0 +1,28 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for German (`de`).
+class AppLocalizationsDe extends AppLocalizations {
+  AppLocalizationsDe([String locale = 'de']) : super(locale);
+
+  @override
+  String get appTitle => 'Curling Companion';
+
+  @override
+  String get login => 'Anmelden';
+
+  @override
+  String get register => 'Registrieren';
+
+  @override
+  String get home => 'Start';
+
+  @override
+  String get events => 'Veranstaltungen';
+
+  @override
+  String get marketplace => 'Marktplatz';
+}
---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,28 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get appTitle => 'Curling Companion';
+
+  @override
+  String get login => 'Login';
+
+  @override
+  String get register => 'Register';
+
+  @override
+  String get home => 'Home';
+
+  @override
+  String get events => 'Events';
+
+  @override
+  String get marketplace => 'Marketplace';
+}
